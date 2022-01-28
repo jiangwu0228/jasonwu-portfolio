1ab0fa1e488543cc9db407612042e39f3eddf9fd
Jason Wu <jiangwu0228@gmail.com>
2022-01-29 02:37:55 +1300

    reinstall app
    dependencies error... did reinstall whole app

---
+++ src/components/topbar/TopBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './topbar.scss';
+
+const TopBar = () => {
+    return (
+        <div className='topbar'>
+            topbar
+        </div>
+    );
+}
+
+export default TopBar;
