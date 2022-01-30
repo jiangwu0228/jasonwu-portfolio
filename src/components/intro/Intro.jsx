89fe7bdbb5241e0b98f7abc56ea721ba3889eea5
Jason Wu <jiangwu0228@gmail.com>
2022-01-30 13:04:20 +1300

    intro component framework

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import "./intro.scss"
+import React from "react";
+import "./intro.scss";
 
 const Intro = () => {
-    return (
-        <div className='intro'>
-            <h1>Intro!</h1>
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="man" />
         </div>
-    );
-}
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Jason Wu</h1>
+          <h3>
+            Freelance<span>Web Developer</span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
