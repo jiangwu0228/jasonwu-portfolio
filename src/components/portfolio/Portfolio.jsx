40cdb5908cdc92f918909c59f47f2a55a0d9ed4d
Jason Wu <jiangwu0228@gmail.com>
2022-02-01 01:27:26 +1300

    portfolio done with editable data in lib.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,44 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./portfolio.scss";
+import {
+  skillCategory,
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../lib/portfolioList";
 
 const Portfolio = () => {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">React</li>
-        <li>Angular</li>
-        <li>Vue</li>
+        {skillCategory.map((item) => {
+          return (
+            <li
+              className={
+                selected === item.id ? "portfolioList active" : "portfolioList"
+              }
+              onClick={() => setSelected(item.id)}
+            >
+              {item.title}
+            </li>
+          );
+        })}
       </ul>
       <div className="container">
-        <div className="item">
-          <img src="./assets/panda.png" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="./assets/panda.png" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="./assets/panda.png" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="./assets/panda.png" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="./assets/panda.png" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="./assets/panda.png" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="./assets/panda.png" alt="" />
-          <h3>Banking App</h3>
-        </div>
+        {data.map((item) => {
+          return (
+            <div className="item">
+              <img src={item.img} alt="" />
+              <h3>{item.title}</h3>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
