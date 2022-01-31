77a74edf900daf2a13fce2c0b921f0d182b26141
Jason Wu <jiangwu0228@gmail.com>
2022-01-31 23:28:09 +1300

    portfolio basic

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
+import "./portfolio.scss";
 
 const Portfolio = () => {
-    return (
-        <div className='portfolio' id='portfolio'>
-            <h1>Portfolio</h1>
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">React</li>
+        <li>Angular</li>
+        <li>Vue</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="./assets/panda.png" alt="" />
+          <h3>Banking App</h3>
         </div>
-    );
-}
+        <div className="item">
+          <img src="./assets/panda.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/panda.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/panda.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/panda.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/panda.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/panda.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Portfolio;
