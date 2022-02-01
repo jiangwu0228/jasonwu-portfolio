9b8a1f85d8bab7eb7ef6c00b378780c9c180df60
Jason Wu <jiangwu0228@gmail.com>
2022-02-02 00:42:34 +1300

    testimonials done

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import "./testimonials.scss"
+import React from "react";
+import "./testimonials.scss";
+import { testimonialsData } from "../../lib/dataList";
 
 const Testimonials = () => {
-    return (
-        <div className='testimonials'>
-            <h1>Testimonials</h1>
-        </div>
-    );
-}
+  return (
+    <div className="testimonials" id="testimonials">
+      <h1>Testimonials</h1>
+      <div className="container">
+        {testimonialsData.map((testimonial) => (
+            <div className={testimonial.featured? "card featured" : "card"}>
+              <div className="top">
+                <img src="assets/right-arrow.png" className="left" alt="" />
+                <img src={testimonial.img} className="user" alt="" />
+                <img src={testimonial.icon} className="right" alt="" />
+              </div>
+                <div className="center">{testimonial.desc}</div>
+                <div className="bottom">
+                  <h3>{testimonial.name}</h3>
+                  <h4>{testimonial.title}</h4>
+                </div>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Testimonials;
