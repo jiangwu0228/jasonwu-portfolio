69eb03c019c70d763938a24f94167353e3eff767
Jason Wu <jiangwu0228@gmail.com>
2022-02-02 01:42:43 +1300

    all frame work done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import "./contact.scss"
+import React, { useState } from "react";
+import "./contact.scss";
 
 const Contact = () => {
-    return (
-        <div className='contact' id='contact'>
-            <h1>Contact</h1>
-        </div>
-    );
-}
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <p>Message sent</p>}
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
