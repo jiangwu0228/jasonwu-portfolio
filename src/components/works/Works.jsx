3d430ea129c68de6d7fbe44ec4e98b612a296e84
Jason Wu <jiangwu0228@gmail.com>
2022-02-16 17:01:06 +1300

    work page adjust

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,20 +1,11 @@
 import React, { useState, useContext } from "react";
 import "./works.scss";
-// import { workData } from "../../lib/dataList";
 import { ThemeContext } from "../../context";
 import { skillsImg } from "../../lib/dataList";
 
 const Works = () => {
   const theme = useContext(ThemeContext);
   const darkTheme = theme.state.darkTheme;
-  // const [currentSlide, setCurrentSlide] = useState(0);
-  // const handleClick = (e) => {
-  //   e === "left"
-  //     ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
-  //     : setCurrentSlide(
-  //         currentSlide < workData.length - 1 ? currentSlide + 1 : 0
-  //       );
-  // };
 
   return (
     <div className={"works " + (darkTheme && "dark")} id="works">
@@ -29,44 +20,6 @@ const Works = () => {
           );
         })}
       </div>
-      {/* <div
-        className="slider"
-        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
-      >
-        {workData.map((data) => {
-          return (
-            <div className="container" key={data.id}>
-              <div className="item">
-                <div className="left">
-                  <div className="leftContainer">
-                    <div className="imgContainer">
-                      <img src={data.icon} alt="" />
-                    </div>
-                    <h2>{data.title}</h2>
-                    <p>{data.desc}</p>
-                    <span>Projects</span>
-                  </div>
-                </div>
-                <div className="right">
-                  <img src={data.img} alt="" />
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <img
-        src="assets/arrow.png"
-        className="arrow left"
-        alt=""
-        onClick={() => handleClick("left")}
-      />
-      <img
-        src="assets/arrow.png"
-        className="arrow right"
-        alt=""
-        onClick={() => handleClick()}
-      /> */}
     </div>
   );
 };
