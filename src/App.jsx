fc3a0e2deaed1afd8140b30b05d828a914eafc90
Jason Wu <jiangwu0228@gmail.com>
2022-02-17 22:38:09 +1300

    menu update

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,17 @@ import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Menu from "./components/menu/Menu";
+import { ThemeContext } from "./context";
 
 import "./app.scss";
 
 function App() {
+  const theme = useContext(ThemeContext);
+  const darkTheme = theme.state.darkTheme;
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="app">
+    <div className={"app " + (darkTheme && "dark")}>
       <TopBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
